67baa2db408c826a2fac39ceabd1f1f22671d952
Florian Koch <fkoch@selectline.de>
2016-10-13 11:37:39 +0200

    Added project

---
+++ DXSample9/DockChildViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace DXSample9
+{
+    public class DockChildViewModel
+    {
+        public DockChildViewModel()
+        {
+            NewCommand = new CommandBinding(ApplicationCommands.New, NewExecute, NewCanExecute);
+            OpenCommand = new CommandBinding(ApplicationCommands.Open, OpenExecute);
+        }
+
+        private void OpenExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Open dialog");
+        }
+
+        private void NewCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = AllowNew;
+        }
+
+        private void NewExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("New dialog");
+        }
+
+        public CommandBinding NewCommand { get; private set; }
+
+        public bool AllowNew { get; set; }
+
+        public CommandBinding OpenCommand { get; private set; }
+    }
+}
---
+++ DXSample9/Views/DockChild.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace DXSample9.Views
+{
+    /// <summary>
+    /// Interaction logic for DockChild.xaml
+    /// </summary>
+    public partial class DockChild
+    {
+        private DockChildViewModel viewModel;
+
+        public DockChild()
+        {
+            InitializeComponent();
+            viewModel = new DockChildViewModel();
+            DataContext = viewModel;
+
+            CommandBindings.Add(viewModel.NewCommand);
+            CommandBindings.Add(viewModel.OpenCommand);
+        }
+    }
+}
